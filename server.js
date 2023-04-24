fddf7440abce991979021688701817078de862d1
satya k <120245070+satyak142@users.noreply.github.com>
2023-04-24 10:13:42 +0530

    setup middleware and server

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
-
+// dependencies
 const express = require('express') ;
 const app = express();
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient ;
 require('dotenv').config();
 
+// monogo connection
+let db,
+    dbConnectionString = process.env.DB_STRING ,
+    dbName = '' ,// add db name
+    collection ;
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('connected to Database');
+        db = client.db(dbName) ;
+        collection = db.collection('')//add collection name
+    })
+
+// view  engine setup
+app.set('view engine','ejs');
+// static files
+app.use(express.static('public'));
+// middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cors());
 
 
 
+// port
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
